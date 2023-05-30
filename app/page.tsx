7705090b7e40c64c4f6762c18a8940df8db0ad5f
jiwon79 <19-079@ksa.hs.kr>
2023-05-30 10:13:30 +0900

    :lipstick: 홈에 있던 캄포넌트 translate 페이지로 이동

--- app/page.tsx
+++ app/page.tsx
@@ -1,61 +1,9 @@
 "use client"
-import styles from './page.module.css'
-import { useState } from "react";
-import useRecorder from "@/lib/hooks/useRecorder";
-import speechTextAPI from "@/lib/api/speechTextAPI";
-import { audioArrayToUrl, blobUrlToBase64 } from "@/lib/utils/function";
-import '@/lib/utils/prototype';
 
-export default function Home() {
-  const {audioURL, isRecording, startRecording, stopRecording,} = useRecorder();
-  const [word, setWord] = useState<string>('');
-  const [audioContent, setAudioContent] = useState<string>('');
-
-  const fetchSTT = async () => {
-    const base64 = await blobUrlToBase64(audioURL);
-
-    return await speechTextAPI.stt(base64);
-  }
-
-  const handleRecording = async () => {
-    if (isRecording) {
-      stopRecording();
-      return;
-    }
-    startRecording();
-  }
-
-  const fetchTTS = async (text: string) => {
-    const response = await speechTextAPI.tts(text);
-    console.log(response.audioContent);
-
-    return response.audioContent;
-  }
-
-  const handleAudioUrl = async () => {
-    const result = await fetchSTT();
-    setWord(result.translate);
-    console.log(result.translate);
-    if (result.translate.isBlank()) {
-      return;
-    }
-    const audio = await fetchTTS(result.translate);
-    const audioUrl = audioArrayToUrl(audio.data);
-    console.log(audioUrl);
-    setAudioContent(audioUrl);
-  }
-
+const Home = () => {
   return (
-    <main className={styles.main}>
-      <h1>녹음 버튼 및 재생</h1>
-      <audio controls src={audioURL}>녹음된 소리를 재생할 audio 엘리먼트</audio>
-      <audio controls src={audioContent}>TTS</audio>
-      <p>{isRecording ? "녹음중" : "녹음 아님"}</p>
-      <p>STT 결과 : {word}</p>
-      <button onClick={handleRecording}>녹음</button>
-      <button onClick={async () => {
-        await handleAudioUrl();
-      }}>음성인식</button>
-    </main>
+    <div>main page</div>
   )
 }
+
+export default Home;
---
+++ app/translate/page.tsx
@@ -0,0 +1,61 @@
+"use client"
+import styles from './page.module.scss';
+import useRecorder from "@/lib/hooks/useRecorder";
+import { useState } from "react";
+import { audioArrayToUrl, blobUrlToBase64 } from "@/lib/utils/function";
+import speechTextAPI from "@/lib/api/speechTextAPI";
+import '@/lib/utils/prototype';
+
+const TranslatePage = () => {
+  const {audioURL, isRecording, startRecording, stopRecording,} = useRecorder();
+  const [word, setWord] = useState<string>('');
+  const [audioContent, setAudioContent] = useState<string>('');
+
+  const handleRecording = async () => {
+    if (isRecording) {
+      stopRecording();
+      return;
+    }
+    startRecording();
+  }
+
+  const fetchSTT = async () => {
+    const base64 = await blobUrlToBase64(audioURL);
+
+    return await speechTextAPI.stt(base64);
+  }
+
+  const fetchTTS = async (text: string) => {
+    const response = await speechTextAPI.tts(text);
+
+    return response.audioContent;
+  }
+
+  const handleAudioUrl = async () => {
+    const result = await fetchSTT();
+    setWord(result.translate);
+    console.log(result.translate);
+    if (result.translate.isBlank()) {
+      return;
+    }
+    const audio = await fetchTTS(result.translate);
+    const audioUrl = audioArrayToUrl(audio.data);
+    setAudioContent(audioUrl);
+  }
+
+  return (
+    <main className={styles.main}>
+      <h1>녹음 버튼 및 재생</h1>
+      <audio controls src={audioURL}>녹음된 소리를 재생할 audio 엘리먼트</audio>
+      <audio controls src={audioContent}>TTS</audio>
+      <p>{isRecording ? "녹음중" : "녹음 아님"}</p>
+      <p>STT 결과 : {word}</p>
+      <button onClick={handleRecording}>녹음</button>
+      <button onClick={async () => {
+        await handleAudioUrl();
+      }}>음성인식</button>
+    </main>
+  );
+}
+
+export default TranslatePage;
