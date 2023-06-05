07aeb4e9018fdec78efdcd75b6971f9c8013ee3e
jiwon79 <19-079@ksa.hs.kr>
2023-06-05 20:43:22 +0900

    :lipstick: main page style

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
-"use client"
-
 import TextInput from "@/components/textInput/TextInput";
+import styles from './page.module.scss';
 
 const Home = () => {
   return (
-    <>
-      <div>main page</div>
-      <TextInput label={"label"} placeholder={'placeholder'} />
-    </>
+    <div className={styles.container}>
+      <p>어떤 상황이신가요?</p>
+      <TextInput label={"장소"} placeholder={'대화가 진행되는 장소를 입력해 주세요.'} />
+      <TextInput label={"상황"} placeholder={'대화 상황을 간단히 설명해 주세요.'} />
+      <button>시작하기</button>
+    </div>
   )
 }
 
