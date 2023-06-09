3b5ef83f9455f3cacec9dc23f2db70544e79b3f9
jiwon79 <19-079@ksa.hs.kr>
2023-06-09 20:14:03 +0900

    :sparkles: 재생 버튼 추가

--- components/Message/Message.tsx
+++ components/Message/Message.tsx
@@ -1,11 +1,20 @@
 import { SpeechState } from "@/lib/recoil/speech";
 import styles from './Message.module.scss';
+import { useRef } from "react";
 
 interface MessageProps {
   speech: SpeechState;
 }
 
 const Message = ({ speech }: MessageProps) => {
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const playAudio = () => {
+    if (!audioRef.current) return;
+
+    audioRef.current.play();
+  }
+
   return (
     <div>
       <div className={styles.container}>
@@ -13,10 +22,12 @@ const Message = ({ speech }: MessageProps) => {
         <p>{speech.translateText === '' ? 'loading' : speech.translateText}</p>
         <p>{speech.reTranslateText === '' ? 'loading' : speech.reTranslateText}</p>
       </div>
+      <audio className={styles.none} src={speech.ttsAudioUrl} ref={audioRef} controls />
       <div>
         <button>수정</button>
         <button>피드백</button>
         <button>삭제</button>
+        <button onClick={playAudio}>재생</button>
       </div>
     </div>
   );
