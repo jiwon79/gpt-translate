ee526bcb79775f1233d3a91fd69e5f90ccec8536
jiwon79 <19-079@ksa.hs.kr>
2023-06-16 00:57:49 +0900

    :sparkles: TextInput 취소 버튼 추가

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,16 @@ const Home = () => {
     setInfo({...info, place: e.target.value});
   }
 
+  const deletePlace = () => {
+    setInfo({...info, place: ''});
+  }
+
   const handleSituation = (e: ChangeEvent<HTMLInputElement>) => {
     setInfo({...info, situation: e.target.value});
+  }
+
+  const deleteSituation = () => {
+    setInfo({...info, situation: ''});
   }
 
   const handlePolite = () => {
@@ -35,12 +43,14 @@ const Home = () => {
         placeholder={'대화가 진행되는 장소를 입력해 주세요.'}
         value={info.place}
         onChange={handlePlace}
+        deleteAction={deletePlace}
       />
       <TextInput
         label={"상황"}
         placeholder={'대화 상황을 간단히 설명해 주세요.'}
         value={info.situation}
         onChange={handleSituation}
+        deleteAction={deleteSituation}
       />
       <ToggleInput label={"높임말"} checked={info.isPolite} onChange={handlePolite}/>
       <div className={styles.spacer}/>
--- components/TextInput/TextInput.tsx
+++ components/TextInput/TextInput.tsx
@@ -3,13 +3,20 @@ import styles from "./TextInput.module.scss";
 
 interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
+  deleteAction?: () => void;
 }
 
-const TextInput = ({ label, ...props }: TextInputProps) => {
+const TextInput = ({label, deleteAction, ...props}: TextInputProps) => {
   return (
     <label className={styles.container}>
       <p className={styles.label}>{label}</p>
-      <input {...props} className={styles.input} />
+      <input {...props} className={styles.input}/>
+      {deleteAction &&
+        <button onClick={deleteAction} className={styles.button}>
+          <div>✕</div>
+        </button>
+      }
+
     </label>
   );
 }
