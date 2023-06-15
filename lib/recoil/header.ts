c431ed69db3e1be986a6780bea6f8d1db5da787e
jiwon79 <19-079@ksa.hs.kr>
2023-06-16 01:19:06 +0900

    :sparkles: header atom 추가

---
+++ lib/recoil/header.ts
@@ -0,0 +1,20 @@
+import { atom, RecoilState } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
+
+export interface Header {
+  label: string;
+  prevHref: string;
+}
+
+const initialHeader = {
+  label: '대화',
+  prevHref: '/',
+}
+
+export const headerAtom: RecoilState<Header> = atom({
+  key: 'header',
+  default: initialHeader,
+  effects_UNSTABLE: [persistAtom],
+});
--- lib/recoil/index.ts
+++ lib/recoil/index.ts
@@ -1,3 +1,4 @@
 export { infoAtom } from "./info";
 export { behaviorAtom } from "./behavior";
 export { dialogListAtom } from "./dialogList";
+export { headerAtom } from "./header";
