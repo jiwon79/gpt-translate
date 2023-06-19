83d9599e616e5007cf15aa9e1e4a4df46e2e093e
jiwon79 <19-079@ksa.hs.kr>
2023-06-19 12:48:18 +0900

    :bug: header 버그 수정

--- app/translate/page.tsx
+++ app/translate/page.tsx
@@ -5,13 +5,21 @@ import ChatWrap from "@/components/ChatWrap/ChatWrap";
 import '@/lib/utils/prototype';
 
 import styles from './page.module.scss';
+import { useEffect } from "react";
+import { useSetRecoilState } from "recoil";
+import { headerAtom } from "@/lib/recoil";
 
 const TranslatePage = () => {
+  const setHeader = useSetRecoilState(headerAtom);
+  useEffect(() => {
+    setHeader({label: '대화', prevHref: '/'});
+  }, [setHeader]);
+
   return (
     <div className={styles.container}>
-      <ChatWrap />
-      <Interaction />
-      <Header />
+      <ChatWrap/>
+      <Interaction/>
+      <Header/>
     </div>
   );
 }
